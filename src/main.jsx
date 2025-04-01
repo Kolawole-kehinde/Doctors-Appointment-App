2be43faa6a63fd54d6dda9f0479ce0c3fa8cabd0
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-04-01 14:21:40 +0100

    fixed routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
 import AppContextProvider from './context/AppContext.jsx'
 import { Toaster } from 'react-hot-toast'
+import { BrowserRouter } from 'react-router'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

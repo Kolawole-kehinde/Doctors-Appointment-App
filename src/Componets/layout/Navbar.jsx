d9e25314ddd30d3d7df344f44710d9c473fa1f98
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-04-02 08:03:19 +0100

    install and configure Supabase client

--- src/Componets/layout/Navbar.jsx
+++ src/Componets/layout/Navbar.jsx
@@ -3,11 +3,11 @@ import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import { useNavigate } from "react-router";
 import Logo from "./logo";
-import CustomButton from "../CustomButton";
 import Menu from "./Menu";
-
+  
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [token, setToken] = useState(true);
   const navigate = useNavigate();
 
   const toggleMenu = () => {
@@ -21,7 +21,29 @@ const Navbar = () => {
        
         <Menu menuStyle="hidden lg:flex items-center font-poppins gap-[1.25rem] text-base leading-[24px] font-medium text-center uppercase" />
        <div className="flex items-center gap-2">
-       <CustomButton />
+         <div className="">
+            {token ? (
+              <div className="flex items-center gap-2 cursor-pointer group relative">
+                <img src="/images/khennycool.jpeg" alt="profile-pic"  className="w-8 rounded-full"/>
+                <IoIosArrowDown fontSize={20}/>
+                  <div className="absolute right-0 top-0 pt-14 text-base font-medium text-secondary-200 z-20  hidden group-hover:block">
+                     <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-2 p-4 capitalize">
+                         <p onClick={() => navigate('profile')} className="hover:text-black cursor-pointer">my Profile</p>
+                         <p onClick={() => navigate('my-appointment')} className="hover:text-black cursor-pointer">my Appointment</p>
+                         <p onClick={() => setToken(false)} className="hover:text-black cursor-pointer">Logout</p>
+                     </div>
+                  </div>
+              </div>
+            ) : (
+              <div   onClick={() => navigate("/auth/register")}
+              className="hidden md:block ">
+                    <button className="w-[195px] h-[54px] bg-primary font-outfit text-lg leading-[43px] text-white text-center rounded cursor-pointer outline-none hover:scale-105 transition-all">
+                Create account
+              </button>
+              </div>
+             
+            )}
+          </div>
         <IoMenu
           fontSize={30}
           className="block md:hidden"
